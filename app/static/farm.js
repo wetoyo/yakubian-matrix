645d28d4c62cf4e993a670659c97de62552befe1
Eviss W <ewu60@stuy.edu>
2026-01-14 21:42:25 -0500

    statics

---
+++ app/static/farm.js
@@ -0,0 +1,6 @@
+const PLANTS = [
+    { id: 'carrot', name: 'Carrot', cost: 5, clickValue: 1, growth: 3, emoji: '🥕' },
+    { id: 'tomato', name: 'Tomato', cost: 25, clickValue: 5, growth: 6, emoji: '🍅' },
+    { id: 'wheat', name: 'Wheat', cost: 100, clickValue: 20, growth: 12, emoji: '🌾' },
+    { id: 'pumpkin', name: 'Pumpkin', cost: 500, clickValue: 120, growth: 30, emoji: '🎃' }
+]
